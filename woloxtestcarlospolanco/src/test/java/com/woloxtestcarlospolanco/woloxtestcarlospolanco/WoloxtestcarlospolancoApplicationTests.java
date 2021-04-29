4db5e9bb46a49448da6db2f05cc4dca0171ebb86
Carlos Polanco <carlos.polanco010@gmail.com>
2021-04-29 16:54:37 -0500

    repo init

---
+++ woloxtestcarlospolanco/src/test/java/com/woloxtestcarlospolanco/woloxtestcarlospolanco/WoloxtestcarlospolancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woloxtestcarlospolanco.woloxtestcarlospolanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoloxtestcarlospolancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
