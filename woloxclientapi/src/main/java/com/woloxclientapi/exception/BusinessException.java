6a9d7ab795460d16bcaac8e5968e455117ecfa46
Carlos Polanco <carlos.polanco010@gmail.com>
2021-05-18 11:28:32 -0500

    delete test

--- woloxclientapi/src/main/java/com/woloxclientapi/exception/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package com.woloxclientapi.exception;
-
-public class BusinessException extends RuntimeException{
-
-	public BusinessException(String reasonPhrase) {
-		super(reasonPhrase);
-	}
-
-}
